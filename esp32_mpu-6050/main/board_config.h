7f5af911f508d3c441e8ec7405fbfd5e7a56718b
vovchik03 <43271103+vovchik03@users.noreply.github.com>
2026-09-04 17:52:10 +0300

    file ctruct on job mit

---
+++ esp32_mpu-6050/main/board_config.h
@@ -0,0 +1,15 @@
+#pragma once 
+
+#define BOARD_I2C_SDA_GPIO      21
+#define BOARD_I2C_SCL_GPIO      22
+#define BOARD_I2C_FREQ_HZ       400000
+#define BOARD_MPU6050_ADDR      0x68
+
+#define BOARD_SERVO_X_GPIO      13
+#define BOARD_SERVO_Y_GPIO      12
+
+#define BOARD_DISP_SPI_MOSI     23
+#define BOARD_DISP_SPI_SCLK     18
+#define BOARD_DISP_SPI_CS       5
+#define BOARD_DISP_DC_GPIO      4
+#define BOARD_DISP_RST_GPIO     2
