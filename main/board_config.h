8f21e74c05e967830f2414d7f7825f2b6bffd72d
vovchik03 <43271103+vovchik03@users.noreply.github.com>
2026-09-13 20:39:21 +0300

    pins try 1

--- main/board_config.h
+++ main/board_config.h
@@ -1,15 +1,15 @@
 #pragma once 
 
-#define BOARD_I2C_SDA_GPIO      21
-#define BOARD_I2C_SCL_GPIO      22
+#define BOARD_I2C_SDA_GPIO      17
+#define BOARD_I2C_SCL_GPIO      16
 #define BOARD_I2C_FREQ_HZ       400000
 #define BOARD_MPU6050_ADDR      0x68
 
-#define BOARD_SERVO_X_GPIO      13
-#define BOARD_SERVO_Y_GPIO      12
+#define BOARD_SERVO_X_GPIO      34
+#define BOARD_SERVO_Y_GPIO      35
 
-#define BOARD_DISP_SPI_MOSI     23
-#define BOARD_DISP_SPI_SCLK     18
-#define BOARD_DISP_SPI_CS       5
+#define BOARD_DISP_SPI_MOSI     13
+#define BOARD_DISP_SPI_SCLK     14
+#define BOARD_DISP_SPI_CS       15
 #define BOARD_DISP_DC_GPIO      4
 #define BOARD_DISP_RST_GPIO     2
